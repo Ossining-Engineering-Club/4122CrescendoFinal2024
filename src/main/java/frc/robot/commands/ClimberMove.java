65d97374e9b40769bffec761ff4e07d1a6f840b4
itserichuang <141202406+itserichuang@users.noreply.github.com>
2024-02-10 09:57:40 -0500

    removed all deprecated imports

--- src/main/java/frc/robot/commands/ClimberMove.java
+++ src/main/java/frc/robot/commands/ClimberMove.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
-import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
