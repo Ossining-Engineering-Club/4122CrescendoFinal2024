06599bf8d7f8ed458db4481a321ee1df720221ec
itserichuang <141202406+itserichuang@users.noreply.github.com>
2024-02-13 20:14:52 -0500

    added 2 second wait in the shooter command

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.constants;
 import frc.robot.subsystems.Shooter;
 
@@ -9,6 +10,7 @@ public class Shoot extends SequentialCommandGroup {
         addCommands(
             new ShooterCommands.SetShooterRPM(shooter, constants.kShooterDefaultRPM),
             new ShooterCommands.FeedToShooter(),
+            new WaitCommand(2),
             new ShooterCommands.SetShooterRPM(shooter, 0.0)
         );
     }
